54d43e8721fb9f19bd67367f1e5d7d16018c4217
himanshu kashyap <kashyapji1908@gmail.com>
2025-11-27 15:30:30 +0530

    first commit

---
+++ todoFrontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-300 font-semibold text-5xl text-center'>
+        Hello Todo
+      </div>
+    </>
+  )
+}
+
+export default App
